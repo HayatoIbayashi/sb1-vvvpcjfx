d4bb67669e1745758164d7a381bb91d095c812c1
HayatoIbayashi <ibayashi@grand-deal.co.jp>
2025-05-25 15:53:27 +0900

    @esbuild-plugins/node-globals-polyfillを追加し、Vite設定を更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,11 +8,19 @@ export default defineConfig({
   plugins: [react()],
   define: {
     // @ts-ignore
-    global: typeof window !== 'undefined' ? window : {},
+    global: {},
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
     include: ['aws-sdk'],
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+          }),
+      ],
+    }
   },
   server: {
     proxy: {
